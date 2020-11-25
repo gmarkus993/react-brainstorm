e34c2d8c1272ff1f769563184b52d2a81ef93e59
Markus George <gmarkus993@gmail.com>
2020-11-25 00:40:35 -0600

    bound a function

--- src/components/app.js
+++ src/components/app.js
@@ -8,17 +8,30 @@ import image from "../../static/assets/images/hoodedMage.jpg"
 
 
 export default class App extends Component {
+  constructor(props){
+    super(props)
+    this.sayHello = this.sayHello.bind(this)
+  }
+  sayHello() {
+    alert('Would you like to login?')
+  }
+
+
   render() {
     return (
       <div className='background' 
         style={{
           backgroundImage: `url(${image})`,
-          imageOrientation: 'from-image',
-          height: '800px',
-          overflow: 'hidden',
+          height: '600px',
+          backgroundPosition: 'center',
+          backgroundSize: 'cover',
+          backgroundRepeat: 'no-repeat'
         
         }}>
         <div className = 'button-group'>
+          <button onClick={this.sayHello} 
+          style={{height: '22px'}}
+          >TEST</button>
           <Login/>
           <Register/>
         </div>
